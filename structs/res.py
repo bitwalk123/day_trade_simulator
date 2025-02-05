1d93c49befd41d3fe5997052159ddd4bb378b315
Fuhito Suguri <bitwalk@gmail.com>
2025-02-06 04:49:30 +0900

    update

--- structs/res.py
+++ structs/res.py
@@ -1,6 +1,7 @@
 class AppRes:
-    dir_image = 'images'
     dir_config = 'conf'
     dir_font = 'fonts'
+    dir_image = 'images'
     dir_ohlc = 'ohlc'
+    dir_result = 'results'
     dir_tick = 'tick'
