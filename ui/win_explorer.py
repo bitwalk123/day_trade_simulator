4ccff30243e556bb55499656bc8ebbdc07408e2a
Fuhito Suguri <bitwalk@gmail.com>
2025-02-06 17:38:39 +0900

    update

--- ui/win_explorer.py
+++ ui/win_explorer.py
@@ -118,7 +118,7 @@ class WinExplorer(QMainWindow):
         info['price_delta_min'] = 0.5
         info['unit'] = 100
 
-        qdate = QDate(2025, 2, 4)
+        qdate = QDate(2025, 2, 5)
 
         self.dict_target = prepDataset(info, qdate, self.res)
 
