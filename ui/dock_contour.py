f8c976b07c5442b3ce74265acd835b91e9343f85
Fuhito Suguri <bitwalk@gmail.com>
2025-02-09 13:14:28 +0900

    update

--- ui/dock_contour.py
+++ ui/dock_contour.py
@@ -7,12 +7,12 @@ from PySide6.QtWidgets import (
     QPushButton,
     QScrollArea,
     QVBoxLayout,
-    QWidget,
+    QWidget, QSlider,
 )
 
 from structs.res import AppRes
 from widgets.buttons import SelectButton
-from widgets.labels import LabelTitle, LabelTitle2
+from widgets.labels import LabelTitle, LabelTitle2, LabelInt
 from widgets.pads import HPadFixed
 
 
@@ -21,7 +21,7 @@ class DockContour(QDockWidget):
         super().__init__()
         self.res = res
 
-        self.setMinimumWidth(300)
+        self.setMinimumWidth(400)
         self.setFeatures(
             QDockWidget.DockWidgetFeature.NoDockWidgetFeatures
         )
@@ -65,16 +65,20 @@ class DockContour(QDockWidget):
         head_name = LabelTitle2('Parameter')
         self.layout_params.addWidget(head_name, r, 0)
 
+        head_value = LabelTitle2('V')
+        head_value.setFixedWidth(30)
+        self.layout_params.addWidget(head_value, r, 1)
+
         head_x = LabelTitle2('X')
         head_x.setFixedWidth(20)
-        self.layout_params.addWidget(head_x, r, 1)
+        self.layout_params.addWidget(head_x, r, 3)
 
         head_y = LabelTitle2('Y')
         head_y.setFixedWidth(20)
-        self.layout_params.addWidget(head_y, r, 2)
+        self.layout_params.addWidget(head_y, r, 4)
 
         pad = HPadFixed()
-        self.layout_params.addWidget(pad, r, 3)
+        self.layout_params.addWidget(pad, r, 5)
 
         r += 1
         self.group_x = group_x = QButtonGroup()
@@ -91,14 +95,24 @@ class DockContour(QDockWidget):
             lab_name = LabelTitle(col)
             self.layout_params.addWidget(lab_name, r, 0)
 
+            lab_value = LabelInt()
+            self.layout_params.addWidget(lab_value, r, 1)
+
+            slider = QSlider(Qt.Orientation.Horizontal)
+            slider.setFixedWidth(100)
+            #slider.setTickPosition(QSlider.TickPosition.TicksBothSides)
+            #slider.setRange(0, 100)
+            #slider.valueChanged.connect(self.show_value)
+            self.layout_params.addWidget(slider, r, 2)
+
             but_x = SelectButton('#44f')
             group_x.addButton(but_x)
-            self.layout_params.addWidget(but_x, r, 1)
+            self.layout_params.addWidget(but_x, r, 3)
 
             but_y = SelectButton('#f44')
             group_y.addButton(but_y)
-            self.layout_params.addWidget(but_y, r, 2)
+            self.layout_params.addWidget(but_y, r, 4)
 
             pad = HPadFixed()
-            self.layout_params.addWidget(pad, r, 3)
+            self.layout_params.addWidget(pad, r, 5)
             r += 1
