fdf780a7665e9c405296f6e3cf408a6b4a1b54d4
Fuhito Suguri <bitwalk@gmail.com>
2025-03-23 07:07:10 +0900

    update

--- widgets/combo.py
+++ widgets/combo.py
@@ -5,6 +5,11 @@ class ComboBox(QComboBox):
     def __init__(self):
         super().__init__()
         self.setStyleSheet("""
+            QComboBox {
+                border: 1px solid gray;
+                border-radius: 3px;
+                padding: 0px 18px 0px 3px;
+            }
             QComboBox:editable {
                 color: black;
                 background: white;
